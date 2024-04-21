ec7070334d4a127dc9db457b5d408729ab85abfa
mari <95420095+mwrina@users.noreply.github.com>
2024-04-21 12:19:05 -0300

    Add link p/ WhatsApp + pequenas correções nos textos

--- js/index.js
+++ js/index.js
@@ -1,3 +1,7 @@
 function redirectSobre (){
     window.location.href = "sobreOInstituto.html";
 };
+
+function redirectWhatsapp() {
+    window.location.href = "https://api.whatsapp.com/send?1=pt_BR&phone=554799605023"
+}
