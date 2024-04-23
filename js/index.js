5f7a596dfd2138b8d4e30cf59bc04165937c5c4e
Nathalia Berri <142815318+nathalia-berri@users.noreply.github.com>
2024-04-23 11:56:41 -0300

    Add files via upload
    - Atualização de algumas imagens
    - Criação escopo dos botões de redirecionamento dos Projetos

--- js/index.js
+++ js/index.js
@@ -5,3 +5,23 @@ function redirectSobre (){
 function redirectWhatsapp() {
     window.location.href = "https://api.whatsapp.com/send?1=pt_BR&phone=554799605023"
 }
+
+function redirectSaibaVoluntarios() {
+
+}
+
+function redirectSaibaEventos() {
+
+}
+
+function redirectSaibaAtendimentoMedico() {
+
+}
+
+function redirectSaibaBrechoOliveira() {
+    
+}
+
+
+/* link icone saiba mais:
+ https://www.citypng.com/photo/21218/share-sharing-white-icon-transparent-background */
